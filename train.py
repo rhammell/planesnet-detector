600776a90d9fa6ac7bc850e0b1cba73a654f6821
Bob Hammell <rhammell@users.noreply.github.com>
2017-06-18 10:34:35 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,54 @@
+import gzip
+import pickle
+import numpy as np
+import tflearn
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# Data loading and preprocessing
+f = gzip.open('planesnet.pklz','rb')
+planesnet = pickle.load(f, encoding='latin1')
+f.close()
+X = planesnet['data'] / 255.
+X = X.reshape([-1,3,20,20]).transpose([0,2,3,1])
+Y = np.array(planesnet['labels'])
+X, Y = shuffle(X, Y)
+Y = to_categorical(Y, 2)
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Real-time data augmentation
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+
+# Convolutional network building
+network = input_data(shape=[None, 20, 20, 3],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+network = conv_2d(network, 32, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = fully_connected(network, 512, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 2, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Train using classifier
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=.2,
+          show_metric=True, batch_size=96, run_id='planesnet_cnn')
+
+# Save model
+model.save("model.tfl")
