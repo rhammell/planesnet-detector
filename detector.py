8517f508d3c894702df1ab0ccba379179df10ba1
Bob Hammell <rhammell@users.noreply.github.com>
2017-06-20 21:20:44 -0400

    Model definition defined in its own module

--- detector.py
+++ detector.py
@@ -1,41 +1,8 @@
 import numpy as np
-import tflearn
-from tflearn.data_utils import shuffle, to_categorical
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.estimator import regression
-from tflearn.data_preprocessing import ImagePreprocessing
-from tflearn.data_augmentation import ImageAugmentation
 from PIL import Image
+from model import model
 
-# Real-time data preprocessing
-img_prep = ImagePreprocessing()
-img_prep.add_featurewise_zero_center()
-img_prep.add_featurewise_stdnorm()
-
-# Real-time data augmentation
-img_aug = ImageAugmentation()
-img_aug.add_random_flip_leftright()
-img_aug.add_random_rotation(max_angle=25.)
-
-# Convolutional network building
-network = input_data(shape=[None, 20, 20, 3],
-                     data_preprocessing=img_prep,
-                     data_augmentation=img_aug)
-network = conv_2d(network, 32, 3, activation='relu')
-network = max_pool_2d(network, 2)
-network = conv_2d(network, 64, 3, activation='relu')
-network = conv_2d(network, 64, 3, activation='relu')
-network = max_pool_2d(network, 2)
-network = fully_connected(network, 512, activation='relu')
-network = dropout(network, 0.5)
-network = fully_connected(network, 2, activation='softmax')
-network = regression(network, optimizer='adam',
-                     loss='categorical_crossentropy',
-                     learning_rate=0.001)
-
-# Load trained model
-model = tflearn.DNN(network, tensorboard_verbose=0)
+# Load in trained model
 model.load("model.tfl")
 
 # Read input image data
@@ -46,26 +13,28 @@ shape = arr.shape
 # Create output image
 output = np.copy(arr)
 
+# Set sliding window params
+step = 30
+win = 20
+
 # Loop through image 
-step = 3
-sz = 20
-for i in range(0, shape[0]-sz, step):
+for i in range(0, shape[0]-win, step):
     print(i)
-    for j in range(0, shape[1]-sz, step):
+    for j in range(0, shape[1]-win, step):
 
         # Extract chip
-        chip = arr[i:i+sz,j:j+sz,:]
+        chip = arr[i:i+win,j:j+win,:]
         
         # Make ML prediction
         prediction = np.argmax(model.predict([chip / 255.])[0])
         
-        # Draw bounding box around detected chips
+        # Draw bounding box around positive detections
         if prediction == 1:
-            output[i:i+sz-1,j,0:3] = [255,0,0]
-            output[i:i+sz-1,j+sz-1,0:3] = [255,0,0]
-            output[i,j:j+sz-1,0:3] = [255,0,0]
-            output[i+sz-1,j:j+sz-1,0:3] = [255,0,0]
+            output[i:i+win, j, 0:3] = [255,0,0]
+            output[i:i+win, j+win-1, 0:3] = [255,0,0]
+            output[i, j:j+win, 0:3] = [255,0,0]
+            output[i+win-1, j:j+win, 0:3] = [255,0,0]
 
 # Save output image
 outIm = Image.fromarray(output)
-outIm.save("scene1_detections.png")
+outIm.save("scene1_detections3.png")
---
+++ model.py
@@ -0,0 +1,35 @@
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Real-time data augmentation
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+
+# Convolutional network building
+network = input_data(shape=[None, 20, 20, 3],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+network = conv_2d(network, 32, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = fully_connected(network, 512, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 2, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Load trained model
+model = tflearn.DNN(network, tensorboard_verbose=0)
